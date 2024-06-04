ff55db0b78f63c460ed73aa42cca735b27332f83
Cecília Levenstein <61717227+cecilialevenstein@users.noreply.github.com>
2024-06-04 19:08:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,4 @@
-let nomeDoHeroi = "Pedro"
-console.log (nomeDoHeroi)
 
-let nivelDoHeroi = 2002;
-let rankDoHeroi = "Prata"
-
-if(nivelDoHeroi >= 1000){
-console.log("Ferro")
-}
-if(nivelDoHeroi >= 1001 && nivelDoHeroi < 2000){
-console.log("Bronze")
-}
-if(nivelDoHeroi >= 2001 && nivelDoHeroi < 5000){
-console.log("Prata")
-}
-if(nivelDoHeroi >= 5001 && nivelDoHeroi < 7000){
-console.log("Ouro")
-}
-if(nivelDoHeroi >= 7001 && nivelDoHeroi < 8000){
-console.log("Platina")
-}
-if(nivelDoHeroi >= 8001 && nivelDoHeroi < 9000){
-console.log("Ascendente")
-}
-if(nivelDoHeroi >= 9001 && nivelDoHeroi < 10000){
-console.log("Imortal")
-}
-if(nivelDoHeroi >= 10001){
-console.log("Radiante")
-}
-
-console.log("O Herói de nome " + nomeDoHeroi + " está no nível de " + nivelRank + ".")
 let nomeDoHeroi = "Pedro"
 
 let nivelDoHeroi = 2001;
